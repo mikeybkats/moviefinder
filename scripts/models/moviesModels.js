87856170f7ae09e5ff701bbb933674d55272eb71
mikeybkats <michaelbkats@gmail.com>
2016-12-20 12:08:59 -0800

    hiding the list of movies

--- scripts/models/moviesModels.js
+++ scripts/models/moviesModels.js
@@ -13,8 +13,8 @@ function Movie(opts){
   this.popularity = opts.popularity;
   this.vote_count = opts.vote_count;
   this.vote_average = opts.vote_average;
-  this.genre_ids = opts.genre_ids;
-  this.movieImage = 'http://image.tmdb.org/t/p/w500'+ opts.backdrop_path;
+  this.genre_ids = opts.genre_ids[0];
+  this.movieImage = 'http://image.tmdb.org/t/p/w500'+ opts.backdrop_path; 
 };
 
 function Genre(opts){
@@ -50,7 +50,7 @@ Movie.fetchAll = function (callback){
 
       appendMoviesList();
       appendMoviesSelection();
-
+      movieListRender();
     }
   });
 
--- scripts/views/movieslistviews.js
+++ scripts/views/movieslistviews.js
@@ -1,12 +1,13 @@
 (function(module) {
   var movieListRenderObj = {};
   movieListRender = function() {
-    for (var i = 10; i < 20; i++) {
+    for (var i = 5; i < 20; i++) {
       $('#topMoviesList').children().eq(i).hide();
       // console.log($('#topMoviesList').children().eq(i).hide());
+      console.log(i);
     }
     $('#movie-data').hide();
   };
-  movieListRender();
+
   module.movieListRenderObj = movieListRenderObj;
 })(window);
